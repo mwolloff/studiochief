c0d0bf0227a48568393cf16d01bbe29f5f4bcaa9
mwolloff <mcwolloff@gmail.com>
2026-04-08 08:01:38 -0700

    Update gunicorn.conf.py

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,8 +1,9 @@
 bind = "0.0.0.0:10000"
-workers = 2
-worker_class = "sync"
-timeout = 120
-graceful_timeout = 120
+workers = 1
+worker_class = "gevent"
+worker_connections = 1000
+timeout = 300
+graceful_timeout = 300
 keepalive = 5
 max_requests = 1000
 max_requests_jitter = 100
